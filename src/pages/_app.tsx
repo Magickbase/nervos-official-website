26f96689ce34d7d567f3328a398c19a131fe316e
WhiteMind <WhiteMind@qq.com>
2023-03-03 16:37:38 +0800

    feat: set ArticulatCF as the default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import { type AppType } from 'next/app'
-
+import localFont from 'next/font/local'
 import { api } from '../utils/api'
 
 import '../styles/globals.css'
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const fontArticulatCF = localFont({
+  src: [
+    { path: '../styles/fonts/ArticulatCF-Normal.woff2', weight: '400' },
+    { path: '../styles/fonts/ArticulatCF-Medium.woff2', weight: '500' },
+    { path: '../styles/fonts/ArticulatCF-DemiBold.woff2', weight: '600' },
+    { path: '../styles/fonts/ArticulatCF-ExtraBold.woff2', weight: '800' },
+  ],
+  preload: true,
+})
+
+const App: AppType = ({ Component, pageProps }) => {
+  return (
+    <main className={fontArticulatCF.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
-export default api.withTRPC(MyApp)
+export default api.withTRPC(App)
