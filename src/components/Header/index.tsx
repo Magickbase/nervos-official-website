97b1d2908044cd73d1bcbb6dd557823dfbff8ed8
WhiteMind <WhiteMind@qq.com>
2023-03-06 20:20:25 +0800

    feat: implement language switch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps, FC } from 'react'
 import clsx from 'clsx'
+import { Popover, Portal } from '@headlessui/react'
+import { useTranslation } from 'next-i18next'
 import styles from './index.module.scss'
 import MenuIcon from './menu.svg'
 import LogoIcon from './logo.svg'
@@ -18,8 +20,59 @@ export const Header: FC<HeaderProps> = props => {
         <MenuIcon />
         <span className={styles.text}>MENU</span>
       </div>
+
       <LogoIcon />
-      <LanguageIcon className={styles.language} />
+
+      <LanguagePopover
+        languages={[
+          { name: 'English', localeName: 'en-US' },
+          { name: '中文', englishName: 'Chinese', localeName: 'zh-CN' },
+          { name: '한국어', englishName: 'Korean', localeName: 'en-US' },
+          { name: 'Türkçe', englishName: 'Turkish', localeName: 'en-US' },
+        ]}
+      />
     </div>
+  )
+}
+
+const LanguagePopover: FC<{
+  languages: {
+    name: string
+    englishName?: string
+    localeName: string
+  }[]
+}> = props => {
+  const { i18n } = useTranslation()
+
+  return (
+    <Popover className={styles.languagePopover}>
+      {({ close }) => (
+        <>
+          <Popover.Button as={'div'} className={styles.trigger}>
+            <LanguageIcon />
+          </Popover.Button>
+
+          {/* Use Portal to prevent being influenced by mix-blend-mode */}
+          <Portal>
+            <Popover.Panel className={styles.languagePopoverContent}>
+              {props.languages.map(language => (
+                <div
+                  key={language.name}
+                  className={styles.language}
+                  // TODO: Maybe it would be better to use link.
+                  onClick={() => {
+                    void i18n.changeLanguage(language.localeName)
+                    close()
+                  }}
+                >
+                  {language.englishName && <div className={styles.englishName}>{language.englishName}</div>}
+                  <div className={styles.name}>{language.name}</div>
+                </div>
+              ))}
+            </Popover.Panel>
+          </Portal>
+        </>
+      )}
+    </Popover>
   )
 }
