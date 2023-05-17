096b7cb4b9e9ebed081bfb8d82e54b2dad7b014e
Keith <keithwhisper@gmail.com>
2023-05-18 03:04:41 +0800

    feat: set zh and es in language navigation available

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -34,11 +34,11 @@ export const Header: FC<HeaderProps> = props => {
       <LanguagePopover
         languages={[
           { name: 'English', localeName: 'en' },
-          { name: '中文', englishName: 'Chinese', localeName: 'zh', disabled: true },
+          { name: '中文', englishName: 'Chinese', localeName: 'zh' },
           // TODO: This is because there is currently no full i18n translation support,
           // so switching to other languages is temporarily disabled.
           { name: '한국어', englishName: 'Korean', localeName: 'ko', disabled: true },
-          { name: 'Español', englishName: 'Spanish', localeName: 'es', disabled: true },
+          { name: 'Español', englishName: 'Spanish', localeName: 'es' },
         ]}
       />
     </div>
