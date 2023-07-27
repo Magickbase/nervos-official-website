e51bc9dd415fc0555df7fd4cbc1e458ef2d72ff8
zhangyouxin <zhangyouxingg@126.com>
2023-07-27 16:30:51 +0800

    feat: i18n for roadmap

--- src/pages/roadmap/index.page.tsx
+++ src/pages/roadmap/index.page.tsx
@@ -1,6 +1,8 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import { Page } from 'src/components/Page'
 import clsx from 'clsx'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 import { StyledLink } from '../../components/StyledLink'
 import { useBodyClass, useIsMobile } from '../../hooks'
 
@@ -12,47 +14,54 @@ import { Section } from './Section'
 
 const Roadmap: NextPage = () => {
   useBodyClass([presets.themeDark ?? ''])
+  const [t] = useTranslation(['roadmap', 'common'])
   const isMobile = useIsMobile()
   const isDesktop = !isMobile
   const sections = [
     {
-      title: <div className={clsx(styles.sectionTitle, styles.inProgressTitle)}>In Progress</div>,
+      title: <div className={clsx(styles.sectionTitle, styles.inProgressTitle)}>{t('in_progress.title')}</div>,
       icon: inprogress,
       iconPosition: 'left' as const,
-      subtitle: 'CKB Hardfork 2023',
-      descriptionText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore magna aliqua. Find out more`,
-      descriptionLink: 'Find out more',
-      projects: ['Kuai', 'ForceRelay', 'Open Transactions', 'Composable Lock Script', 'CKB Light Client'],
+      subtitle: t('in_progress.subtitle'),
+      descriptionText: t('in_progress.description'),
+      descriptionLink: t('in_progress.link'),
+      projects: [
+        t('in_progress.kuai'),
+        t('in_progress.force_relay'),
+        t('in_progress.open_transactions'),
+        t('in_progress.composable_lock_script'),
+        t('in_progress.ckb_light_client'),
+      ],
     },
     {
-      title: <div className={clsx(styles.sectionTitle, styles.upcomingTitle)}>Upcoming</div>,
+      title: <div className={clsx(styles.sectionTitle, styles.upcomingTitle)}>{t('upcoming.title')}</div>,
       icon: upcoming,
       iconPosition: 'right' as const,
-      subtitle: '2023 Hardtork',
-      descriptionText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore magna aliqua. Find out more`,
-      descriptionLink: 'Find out more',
-      projects: ['Light client soft fork'],
+      subtitle: t('upcoming.subtitle'),
+      descriptionText: t('upcoming.description'),
+      descriptionLink: t('upcoming.link'),
+      projects: [t('upcoming.light_client')],
     },
     {
-      title: <div className={clsx(styles.sectionTitle, styles.achievementsTitle)}>Achievements</div>,
+      title: <div className={clsx(styles.sectionTitle, styles.achievementsTitle)}>{t('achievements.title')}</div>,
       icon: achievements,
       iconPosition: 'left' as const,
-      subtitle: 'SUDT',
-      descriptionText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore magna aliqua. Find out more`,
-      descriptionLink: 'Find out more',
+      subtitle: t('achievements.description'),
+      descriptionText: t('achievements.description'),
+      descriptionLink: t('achievements.link'),
       projects: [
-        'Token.citv (CoTA)',
-        'Light client soft fork',
-        'Light client implementation',
-        'Neuron',
-        'Axon beta',
-        'Godwoken Launches',
-        'Godwoken v1 Ethereum Equivalence',
-        'JoyID testnet',
-        '.bit launches',
-        'Force bridge launch',
-        'Opera integration',
-      ],
+        t('achievements.cota'),
+        t('achievements.light_client_soft_fork'),
+        t('achievements.light_client_implementation'),
+        t('achievements.neuron'),
+        t('achievements.axon_beta'),
+        t('achievements.godwoken_launches'),
+        t('achievements.godwoken_v1_ethereum_equivalence'),
+        t('achievements.joyid_testnet'),
+        t('achievements.bit_launches'),
+        t('achievements.force_bridge_launch'),
+        t('achievements.opera_integration'),
+      ]
     },
   ]
 
@@ -67,42 +76,36 @@ const Roadmap: NextPage = () => {
         )}
         {isMobile && (
           <>
-           <div className={clsx(styles.embellishedElements, styles.embellishedElementMobileUp)} />
-           <div className={clsx(styles.embellishedElements, styles.embellishedElementMobileDown)} />
-           </>
+            <div className={clsx(styles.embellishedElements, styles.embellishedElementMobileUp)} />
+            <div className={clsx(styles.embellishedElements, styles.embellishedElementMobileDown)} />
+          </>
         )}
         <div className={styles.content}>
-          <div className={styles.title}>The journey</div>
+          <div className={styles.title}>{t('title')}</div>
           <div className={styles.subTitleWrapper}>
-            <div className={styles.subTitleItem}>NEVER FINISHED,</div>
-            <div className={styles.subTitleItem}>ALWAYS BUILDING</div>
+            <div className={styles.subTitleItem}>{t('subtitle.never_finished')},</div>
+            <div className={styles.subTitleItem}>{t('subtitle.always_building')}</div>
           </div>
 
           <div className={styles.badges}>
             <Badges />
           </div>
           <div className={styles.slogan}>
-            <div className={styles.sloganTitle}>Get involved</div>
-            <div className={styles.sloganText}>
-              Nervos is an open source project to build the primitives necessary for a robust, permissionless digital
-              economy. The path to realize this ambition is windy, full of quiet triumps and surprising turns.
-            </div>
-            <div className={styles.sloganText}>
-              Above all, Nervos is a community-driven endeavor, and anything that is accomplished is done through a
-              galvanized genuine interest in the brilliant minds that contribute to the network&apos;s development.
-            </div>
+            <div className={styles.sloganTitle}>{t('get_involved.title')}</div>
+            <div className={styles.sloganText}>{t('get_involved.description_1')}</div>
+            <div className={styles.sloganText}>{t('get_involved.description_2')}</div>
             <StyledLink className={styles.link} href={''} colored>
-              RFCs
+              {t('get_involved.rfcs')}
             </StyledLink>{' '}
             |{' '}
             <StyledLink className={styles.link} href={''} colored>
-              Nervos Talk Forum
+              {t('get_involved.nervos_talk_forum')}
             </StyledLink>
           </div>
           {sections.map((section, index) => {
-            if(isDesktop && index === 1) {
+            if (isDesktop && index === 1) {
               // when on desktop, background color for the second section is different
-              return  <Section key={section.subtitle} {...section} className={styles.darkerBg}/>
+              return <Section key={section.subtitle} {...section} className={styles.darkerBg} />
             }
             return <Section key={section.subtitle} {...section} />
           })}
@@ -110,6 +113,15 @@ const Roadmap: NextPage = () => {
       </Page>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale = 'en' }) => {
+  const lng = await serverSideTranslations(locale, ['common', 'roadmap'])
+  return {
+    props: {
+      ...lng,
+    },
+  }
 }
 
 export default Roadmap
